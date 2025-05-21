8f9c799cc7c5b10d9d99d079202c0f6988f14866
Titouan Bonnet Plomb <titouan.plomb@outlook.com>
2025-05-21 11:53:33 +0200

    Initial commit

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from database import Base
 from datetime import datetime
 
 class ItemDB(Base):
-    __tablename__ = "Products"
+    __tablename__ = "Orders"
     
     name = Column(String, index=True)
     price = Column(Float)
@@ -11,3 +11,4 @@ class ItemDB(Base):
     color = Column(String)
     stock = Column(Integer)
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    order_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
